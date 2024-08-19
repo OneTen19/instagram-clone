2ffbcee87307d8fe2d668f1c2a90db92f32b2dd2
OneTen19 <khy000426@naver.com>
2024-08-19 14:18:11 +0900

    Initial Commit

---
+++ InstagramClone/InstagramCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramCloneApp.swift
+//  InstagramClone
+//
+//  Created by OneTen on 8/19/24.
+//
+
+import SwiftUI
+
+@main
+struct InstagramCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
