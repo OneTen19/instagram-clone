102a0f97c0df72ef02fb3bd117d3b4e3f9cadd07
OneTen19 <khy000426@naver.com>
2024-08-22 16:20:37 +0900

    User data Model

---
+++ InstagramClone/Model/User.swift
@@ -0,0 +1,27 @@
+//
+//  User.swift
+//  InstagramClone
+//
+//  Created by OneTen on 8/22/24.
+//
+
+import Foundation
+
+struct User: Identifiable, Codable {
+    let id: String
+    var username: String
+    var profileImageUrl: String?
+    var fullname: String?
+    var bio: String?
+    let email: String
+    
+}
+
+extension User {
+    static var MOCK_USERS: [User] = [
+        .init(id: NSUUID().uuidString, username: "user01", profileImageUrl: "user01", fullname: "user01 fullname", bio: "user01 bio", email: "user01@naver.com"),
+        .init(id: NSUUID().uuidString, username: "user02", profileImageUrl: "user02", fullname: "user02 fullname", bio: "user02 bio", email: "user02@naver.com"),
+        .init(id: NSUUID().uuidString, username: "user03", profileImageUrl: "user03", fullname: "user03 fullname", bio: "user03 bio", email: "user03@naver.com"),
+        .init(id: NSUUID().uuidString, username: "user04", profileImageUrl: "user04", fullname: "user04 fullname", bio: "user04 bi0", email: "user04@naver.com")
+    ]
+}
